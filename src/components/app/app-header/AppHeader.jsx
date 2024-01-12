f5474d4f119e8eb35e9af7e8e3ab0e3d9043c78d
o3-noone <jaloliddintuitdif315@gmail.com>
2024-01-12 21:25:08 +0500

    ok

--- src/components/app/app-header/AppHeader.jsx
+++ src/components/app/app-header/AppHeader.jsx
@@ -1,19 +1,25 @@
 import "./appHeader.css";
+import baza from "../../baza/Baza";
 function AppHeader() {
   return (
     <div>
       <div className="appHeader">
         <div className="userInfo">
+          <div className="searchBox">
+            <input type="search" required placeholder="Search" />
+          </div>
           <div className="userList">
-            <div className="userImg">
-              <img src="" alt="" />
-            </div>
-            <div className="userText">
-              <h3></h3>
-              <p></p>
-            </div>
-            <div className="userCardPrice">
-              <h4>300 000</h4>
+            <div className="userItem">
+              <div className="userImg">
+                <img src="" alt="" />
+              </div>
+              <div className="userText">
+                <h3>Mirzayev Jaloliddin Kamoliddin o'g'li </h3>
+                <p>+998912345678</p>
+              </div>
+              <div className="userCardPrice">
+                <h4>limit 300 000 so'm</h4>
+              </div>
             </div>
           </div>
         </div>
--- src/components/baza/Baza.js
+++ src/components/baza/Baza.js
@@ -5,22 +5,6 @@ const baza=[
       rool: "Admin",
       username: "karzinka",
       password: "karzinka",
-      items: [
-        {
-          id:1,
-          name: "sut",
-          firstData: "11.10.2020",
-          lastData: "11.10.2022",
-          type: "sut maxsuloti"
-        },
-        {
-          id:2,
-          name: "shokalad",
-          firstData: "11.10.2021",
-          lastData: "11.10.2023",
-          type: "shirinlik"
-        }
-      ]
     }
   ]
   export default baza
